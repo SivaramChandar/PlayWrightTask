7b206d73f187c333500f8b71de311776eed563b8
SivaramChandar <sivachandru97@gmail.com>
2026-02-02 21:15:32 +0530

    Add files via upload

---
+++ Week3day1assign.spec.ts
@@ -0,0 +1,11 @@
+import {test} from "@playwright/test"
+import { parseArgs } from "node:util"
+
+test("Browser login", async({page})=>{
+    await page.goto("https://login.salesforce.com/?locale=in")
+    await page.locator("#username").fill("dilipkumar.rajendran@testleaf.com")
+    await page.locator("#password").fill("TestLeaf@2025")
+    await page.locator("#Login").click()
+    await page.waitForTimeout(12000)
+    await page.locator("button[title='App Launcher']").click()
+})
